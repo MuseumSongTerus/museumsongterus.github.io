21c4bf222349e08f794da0720b168241fd35ad11
MuseumSongTerus <songterus12@gmail.com>
2024-11-26 21:29:01 +0700

    Update score.js
    Major changes on js file

--- score/score.js
+++ score/score.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
     const correctAnswers = localStorage.getItem('correctAnswers') || 0; // Get the score from localStorage
     $('#final-score').text(correctAnswers); // Display the score on the final score page
+
+    if (correctAnswers >= 8) {
+		$('#score-title').text('Selamat!');
+        $('#souvenir').text('Tunjukan skor ini ke resepsionis dan dapatkan suvenirnya.');
+        $('img').attr('src', 'images/berhasil.png'); // Update image source
+        $('img').attr('alt', 'Berhasil'); // Update alt text
+    } else {
+		$('#score-title').text('Anda kurang beruntung!');
+        $('#souvenir').text('Jawab dengan benar minimal 8 pertanyaan untuk mendapatkan suvenir di resepsionis.');
+        $('img').attr('src', 'images/gagal.png'); // Update image source
+        $('img').attr('alt', 'Gagal'); // Update alt text
+    }
 });
